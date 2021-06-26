d00d87380486c1fef24a74b3b3ad9f5393b66ebc
uncoolbeans <ivcleandedmyroom@gmail.com>
2021-06-26 11:18:46 +0800

    Added descriptions to FriendDetailView.

--- FriendsApp/ContentView.swift
+++ FriendsApp/ContentView.swift
@@ -8,12 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    var friends = [Friend(name: "YJ",
+                          icon: "zzz",
+                          school: "Tinkercademy",
+                          slothImage: "sloth3",
+                          description: "nutella nutella nutella 🌰"),
+                   Friend(name: "Jia Chen",
+                          icon: "swift",
+                          school: "Ngee Ann Poly",
+                          slothImage: "sloth2", description: "i ❤️ xcodee 🧑🏼‍💻"),
+                          
+                   Friend(name: "Zerui",
+                          icon: "wifi",
+                          school: "NUS High",
+                          slothImage: "sloth1",
+                          description: "wifi is no more 🚫")]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(friends) { friend in
+                NavigationLink(destination: FriendDetailView(friend: friend)) {
+                Image(systemName: friend.icon)
+                
+                VStack(alignment: .leading) {
+                    Text(friend.name).bold()
+                    Text(friend.school)
+                    }
+                }
+            }.navigationTitle("Friends")
+        }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
---
+++ FriendsApp/Friend.swift
@@ -0,0 +1,16 @@
+//
+//  Friend.swift
+//  FriendsApp
+//
+//  Created by MMS on 26/6/21.
+//
+
+import Foundation
+struct Friend: Identifiable {
+    var id = UUID()
+    var name: String
+    var icon: String
+    var school: String
+    var slothImage: String
+    var description: String
+}
